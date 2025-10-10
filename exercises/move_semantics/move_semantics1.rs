7610e37049da52f97652c9e6b470c89f0941e9a1
zhangjiawe2 <zhang@LAPTOP-KJN0MMVB.localdomain>
2025-10-10 16:27:31 +0800

    update

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -16,8 +16,7 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
 
     vec.push(22);
     vec.push(44);
